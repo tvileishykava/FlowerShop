ffe15f64988ed199e20166fd24e0d36beb198b46
Tatsiana Vileishykava <ledy6katanya@gmail.com>
2019-11-24 14:39:49 +0300

    renew24112019_2_

--- FlowerShop/Program.cs
+++ FlowerShop/Program.cs
@@ -14,43 +14,9 @@ namespace FlowerShop
     {
         static void Main(string[] args)
         {
-
-
-            //Rose Rose1 = new Rose("Rose", 3.00, 30);
-            //Rose Rose2 = new Rose("Rose", 3.00);
-            //Rose1.DisplayFlower();
-
-            //FlowerSet FlowerSet1 = new FlowerSet();
-            //FlowerSet1.AddFlowerToFlowerCollection("Astra", 3, "Astra", 3.00);
-            //FlowerSet1.AddFlowerToFlowerCollection("Rose", 2, "Rose", 5.00, 30);
-            //FlowerSet1.SortFlowersInFlowerSetBySmth();
-
-            //Menu menu = new Menu();
-            //menu.DisplayAvailableFlowers();
-
-            /*FlowerSet1.DeleteFlowerFromFlowerCollection("Astra", 1);
-
-
-            FlowerSet1.DisplayTotalPrice();
-
-
-            FlowerSet1.DisplayTotalPrice(20);
-            FlowerSet1.FindFlowerinFlowerSetByPrice(0, 3.5);
-
-
-            Console.WriteLine("___________");
-            FlowerSet1.RemoveAllFlowersFromFlowerCollection();
-            FlowerSet1.DisplayFlowerNamesAndPriceAndQuantity();
-            FlowerSet1.DisplayTotalPrice();
-            FlowerSet1.DisplayTotalPrice(20);*/
+                       
             Menu menu = new Menu();
             menu.ShowMainMenu();
-            //flSet.DisplayFlowerNamesAndPriceAndQuantity();
-
-
-
-
-            //do not delete, useful line
             Console.ReadLine();
              
         }
